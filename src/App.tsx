ba4561306fce57f106c21956c3b55f99f70647d3
Tor Arve Stangeland <tor.stangeland@bouvet.no>
2022-05-24 12:01:35 +0200

    Add authentication

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,36 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { MsalAuthenticationTemplate, useMsal } from '@azure/msal-react';
+import { InteractionType } from '@azure/msal-browser';
+
+const authRequest = {
+  scopes: ["User.Read"],
+};
 
 function App() {
+  const { instance } = useMsal();
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <MsalAuthenticationTemplate
+    interactionType={InteractionType.Redirect}
+    authenticationRequest={authRequest}
+    >
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Logged in as { instance.getActiveAccount()?.username || 'none' }</p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    </MsalAuthenticationTemplate>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -3,13 +3,47 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthenticationResult, Configuration, EventMessage, EventType, PublicClientApplication } from '@azure/msal-browser';
+import { MsalProvider } from '@azure/msal-react';
+
+const redirectUri = () => {
+  const location = document.location;
+  if (location.hostname === 'localhost') {
+    return `${location.protocol}//${location.hostname}:${location.port}${location.pathname}`
+  }
+  
+  return `${location.protocol}//${location.hostname}${location.pathname}`
+}
+const configuration: Configuration = {
+  auth: {
+    clientId: 'f25898ed-4118-46ab-b231-49d16e4c5c6a',
+    authority: 'https://login.microsoftonline.com/625ef37f-e858-4339-9db6-5cb143aead1a',
+    redirectUri: redirectUri()
+  },
+}
+
+const pca = new PublicClientApplication(configuration);
+
+const accounts = pca.getAllAccounts();
+if (accounts.length > 0) {
+  pca.setActiveAccount(accounts[0]);
+}
+pca.addEventCallback((event: EventMessage) => {
+if (event.eventType === EventType.LOGIN_SUCCESS && event.payload) {
+    const payload = event.payload as AuthenticationResult;
+    const account = payload.account;
+    pca.setActiveAccount(account);
+}
+});
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <MsalProvider instance={pca}>
+      <App />
+    </MsalProvider>
   </React.StrictMode>
 );
 
